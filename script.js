6e5c1dbfc262d6553c61ac1c67450cc8a35a2573
muneerapatel <patel.muneera@gmail.com>
2022-10-09 23:07:31 -0400

    Display relevant weather icon from API

--- script.js
+++ script.js
@@ -16,8 +16,12 @@ function displayWeather(response) {
   document.querySelector(".high").innerHTML = Math.round(
     response.data.main.temp_max
   );
-  document.querySelector(".currentWeatherImage").innerHTML =
-    response.data.main.temp;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather.description);
 }
 
 // city display
