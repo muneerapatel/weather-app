c63cd445b1c1bd4e0bbde76c18a52ab4f66c0099
muneerapatel <patel.muneera@gmail.com>
2022-10-14 14:50:28 -0400

    Replace placeholder hours with API hours

--- script.js
+++ script.js
@@ -74,32 +74,60 @@ day.innerHTML = enteredDay + " ";
 hour.innerHTML = enteredHour;
 minute.innerHTML = enteredMinute + " ";
 
-//current hourly forecast
-
-function displayForecast() {
-  let forecastElement = document.querySelector(".dayForecast");
+function formatHour(hourstamp) {
+  let date = new Date(hourstamp * 1000);
+  let hour = date.getHours();
   let hours = [
     "12 AM",
+    "1 AM",
+    "2 AM",
     "3 AM",
+    "4 AM",
+    "5 AM",
     "6 AM",
+    "7 AM",
+    "8 AM",
     "9 AM",
+    "10 AM",
+    "11 AM",
     "12 PM",
+    "1 PM",
+    "2 PM",
     "3 PM",
+    "4 PM",
+    "5 PM",
     "6 PM",
+    "7 PM",
+    "8 PM",
     "9 PM",
+    "10 PM",
     "11 PM",
   ];
+  return hours[hour];
+}
+//current hourly forecast
+
+function displayForecast(response) {
+  let forecast = response.data.hourly;
+  let forecastElement = document.querySelector(".dayForecast");
+
   let forecastHTML = `<div class="row justify-content-center">`;
-  hours.forEach(function (hour) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastHour, index) {
+    if (index < 10) {
+      forecastHTML =
+        forecastHTML +
+        `
           <div class="col-1">
-            <div class="hourForecast">${hour}</div>
-            <img src="http://openweathermap.org/img/wn/09n@2x.png" alt="" width="60"/>
-            <div class="hourlyTemperature">18°</div>
+            <div class="hourForecast">${formatHour(forecastHour.dt)}</div>
+            <img src="http://openweathermap.org/img/wn/${
+              forecastHour.weather[0].icon
+            }@2x.png" alt="" width="60"/>
+            <div class="hourlyTemperature">${Math.round(
+              forecastHour.temp
+            )}</div>
       </div>
       `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
@@ -108,9 +136,7 @@ function displayForecast() {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "c9a673fc2e2ccbea1cacfb6a4a47e6f7";
-  let apiUrl = `
-https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric
-`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=e0a5a97de9a0b7a951e9d154a8f9bad8&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -132,9 +158,8 @@ function displayCelciusTemperature(event) {
   let temperatureElement = document.querySelector("#temp-reading");
   temperatureElement.innerHTML = Math.round(celciusTemperature);
 }
-let celciusTemperature = null;
 
-displayForecast();
+let celciusTemperature = null;
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
